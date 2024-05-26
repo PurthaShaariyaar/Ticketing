299c3908abb0e9fc05f0c327723e86c0f2ce5bcd
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-05-26 00:31:31 -0400

    Sign up flow completed.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -14,7 +15,14 @@ import { NotFoundError } from './errors/not-found-error';
 const PORT = 4000;
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+)
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -28,6 +36,11 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined.');
+  }
+
   try {
     await mongoose.connect('mongodb://auths-mongo-srv:27017/auths');
 
